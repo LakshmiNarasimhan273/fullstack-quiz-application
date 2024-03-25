30445038b043148c4b5dbc64611503f51d67435d
Sriram511 <bachelorofphotography@gmail.com>
2024-03-25 15:15:19 +0530

    Final Modification done and ready to deploy

--- Components/Result.js
+++ Components/Result.js
@@ -22,41 +22,46 @@ const Result = ({ username }) => {
 
     setResult(correctAnswers);
   }, [stats.UserArray]);
-  
+
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-
   return (
     <div className="flex flex-col items-center justify-center p-4">
       <div className="mb-4">
-        <p className="text-3xl font-bold text-Black-500">{capitalizeFirstLetter(username)}</p>
+        <p className="text-3xl font-bold text-Black-500">
+          {capitalizeFirstLetter(username)}
+        </p>
       </div>
 
       <div className="bg-gray-200 text-white p-6 space-x-5 flex justify-between items-center rounded-lg mb-6 w-full">
-        <p className="text-4xl">Result {Math.floor((result / API.length) * 100)}%</p>
+        <p className="text-4xl text-black">
+          Result {Math.floor((result / API.length) * 100)}%
+        </p>
         <div className="bg-gold-400 rounded-full h-20 w-20 flex justify-center items-center overflow-hidden">
           <p
             style={{
-              background: `conic-gradient(#8B4513 0 ${(result / API.length) * 100}%, transparent 0 0%)`,
+              background: `conic-gradient(#2D5AFF 0 ${
+                (result / API.length) * 100
+              }%, transparent 0 0%)`,
             }}
             className="w-full h-full flex items-center justify-center"
           ></p>
         </div>
       </div>
 
-      <div className="bg-gray-400 text-white p-6 flex justify-between text-center rounded-lg mb-6 w-full">
+      <div className="bg-gray-200 text-white p-6 flex justify-between text-center rounded-lg mb-6 w-full">
         <div>
-          <p className="text-lg">Total</p>
-          <p className="text-4xl">{API.length}</p>
+          <p className="text-lg text-black">Total</p>
+          <p className="text-4xl text-black">{API.length}</p>
         </div>
         <div>
-          <p className="text-lg ">Correct</p>
+          <p className="text-lg text-black ">Correct</p>
           <p className="text-4xl text-green-600">{result}</p>
         </div>
         <div>
-          <p className="text-lg">Wrong</p>
+          <p className="text-lg text-black">Wrong</p>
           <p className="text-4xl  text-red-700">{API.length - result}</p>
         </div>
       </div>
